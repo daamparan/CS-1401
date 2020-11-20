2e2d09da7874394ffb62ff3db0231784b2623734
daamparan <43897297+daamparan@users.noreply.github.com>
2020-11-20 00:23:03 -0700

    Delete README.m

--- CS 3432: Comp. Arch 1/Project 2- Blinky Buzzy Toy/README.m
+++
@@ -1,9 +0,0 @@
-# This is a copy
-# Project 2 - Blinky Buzzy Toy
-
-After being introduced to the C language and building a simple project that mainly dealt with pointers.
-By using pointers we were introduced to pointer arithmentic and the general concept of these which led us 
-to begin diving into assembly code and the translation between high level code to low level. 
-
-Our first major project and my first use of actual hardware are demonstrated here. Much like Project 1 there 
-is code provided by Dr. Freudenthal. 
